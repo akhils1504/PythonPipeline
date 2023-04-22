75703c51bc753c9105f63ae59fc64eada4489896
Kumar S <Akhil.KumarS@finastra.com>
2023-04-22 15:24:49 +0530

    code

---
+++ readwrite.py
@@ -0,0 +1,9 @@
+#file = open("samplefile.txt")
+#print(file.read())  # read all contents
+#print(file.read(4))  # read n no of character
+#print(file.readline())# print single line at a time
+#print(file.readline(2))# read 2 lines in text file
+
+#file.close()
+
+print("Hellloooo")
